da06ef95142cc803da605fd53364f7caef322834
Rony Varghese <rony@mobme.in>
2011-08-25 12:02:30 +0530

    Deleted everything

--- first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '14b52a9a15e5558d01fc2e38a954c4b7a6ba5a2d9010fb9b6877a17da2cc1b1e35e83085072c0cf336c47d7a068b5d2419f01242178b52469705099137bc8354'
